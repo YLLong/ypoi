21f9c61ac17b7f119c5ec2876dfa3542f9c8eea9
oDragon.yan <yanlinlung@163.com>
2018-11-12 23:41:55 +0800

    此项目第一次提交

---
+++ src/main/java/com/yll/ypoi/YpoiApplication.java
@@ -0,0 +1,13 @@
+package com.yll.ypoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpoiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YpoiApplication.class, args);
+        System.out.println("hello world");
+    }
+}
